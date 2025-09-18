519de75ab9a38f2d9e6b2ada1cebe7839e186bc0
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-09-18 09:21:23 -0500

    Pantalla de producto

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ProductsResponse } from '@products/interfaces/product.interface';
+import {
+  Product,
+  ProductsResponse,
+} from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -29,6 +32,12 @@ export class ProductsService {
           gender,
         },
       })
+      .pipe(tap((resp) => console.log(resp)));
+  }
+
+  getProductByIdSlug(idSlug: string): Observable<Product> {
+    return this.http
+      .get<Product>(`${baseUrl}/products/${idSlug}`)
       .pipe(tap((resp) => console.log(resp)));
   }
 }
--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  private readonly productsService = inject(ProductsService);
+  private readonly activatedRouter = inject(ActivatedRoute);
+
+  private readonly params = toSignal(this.activatedRouter.params);
+
+  productIdSlug = this.params()?.['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({ productIdSlug: this.productIdSlug }),
+    loader: ({ request }) =>
+      this.productsService.getProductByIdSlug(request.productIdSlug),
+  });
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.productIdSlug);
+  }
+}
