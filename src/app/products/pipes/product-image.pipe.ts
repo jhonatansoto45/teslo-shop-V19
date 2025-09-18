dc8b356d6bc3647b0ca64c69a4386b0355ee24e1
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-09-18 09:03:46 -0500

    ProductImage Pipe - Pipe Personalizado

---
+++ src/app/products/pipes/product-image.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+@Pipe({
+  name: 'productImage',
+})
+export class ProductImagePipe implements PipeTransform {
+  transform(value: string | string[]): string {
+    if (typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
+    }
+
+    if (Array.isArray(value) && value.length > 1) {
+      const image = value.at(0);
+      return `${baseUrl}/files/product/${image}`;
+    }
+
+    return './assets/images/no-image.jpg';
+  }
+}
