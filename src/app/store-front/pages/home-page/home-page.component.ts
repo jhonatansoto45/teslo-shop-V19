e7084c11e8c09d4c4a49b9682809185afa6c1503
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-09-19 08:34:34 -0500

    Paginación con RxResource

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
-import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -11,11 +13,24 @@ import { PaginationComponent } from "@shared/components/pagination/pagination.co
 })
 export class HomePageComponent {
   private readonly productsService = inject(ProductsService);
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  readonly currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map((params) => (params.get('page') ? +params.get('page')! : 1)),
+      map((page) => (isNaN(page) ? 1 : page))
+    ),
+    {
+      initialValue: 1, // si viene null el valor de arriba se inicializa el signal con `1`
+    }
+  );
 
   readonly productResource = rxResource({
-    request: () => ({}),
+    request: () => ({ page: this.currentPage() - 1 }),
     loader: ({ request }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: request.page * 9,
+      });
     },
   });
 }
