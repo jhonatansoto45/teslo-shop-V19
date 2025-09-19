348f0ce2f58f14218b7f33200bea455359e910a4
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-09-19 08:26:26 -0500

    Paginación local - Funcionalidad

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,13 +1,16 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
 })
 export class PaginationComponent {
   readonly pages = input(0);
   readonly currentPage = input(1);
+
+  readonly activePage = linkedSignal(this.currentPage);
 
   readonly getPagesList = computed(() => {
     return Array.from({ length: this.pages() }, (_, i) => i + 1);
