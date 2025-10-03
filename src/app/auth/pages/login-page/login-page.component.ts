f9884b5b5cbb7dcbb3145908643a8bcac7bf7e63
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-10-03 14:25:35 -0500

    Formulario reactivo de autenticación

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {
+  private readonly fb = inject(FormBuilder);
+  readonly hasError = signal(false);
+  readonly isPosting = signal(false);
+
+  readonly loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  submit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+    console.log({ email, password });
+  }
+}
