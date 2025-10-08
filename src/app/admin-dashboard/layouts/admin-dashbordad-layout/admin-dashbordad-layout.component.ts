ea87ca56bd1355d820e9a78a5a715f5894a833e4
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-10-08 08:11:34 -0500

    Diseño del panel administrativo

--- src/app/admin-dashboard/layouts/admin-dashbordad-layout/admin-dashbordad-layout.component.ts
+++ src/app/admin-dashboard/layouts/admin-dashbordad-layout/admin-dashbordad-layout.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
+
 
 @Component({
   selector: 'app-admin-dashbordad-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashbordad-layout.component.html',
 })
-export class AdminDashbordadLayoutComponent { }
+export class AdminDashbordadLayoutComponent {
+  readonly authService = inject(AuthService);
+
+  readonly user = computed(() => this.authService.user())
+}
