4eb51d24513abfd6e439610ed751777b04dfb0e6
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2025-10-09 09:09:38 -0500

    Actualizar caché

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -69,7 +69,26 @@ export class ProductsService {
     );
   }
 
-  updateProduct(id: string, productLike: Partial<Product>): Observable<Product> {
-    return this.http.patch<Product>(`${baseUrl}/products/${id}`, productLike);
+  updateProduct(
+    id: string,
+    productLike: Partial<Product>
+  ): Observable<Product> {
+    return this.http
+      .patch<Product>(`${baseUrl}/products/${id}`, productLike)
+      .pipe(tap((product) => this.updateProductCache(product)));
+  }
+
+  updateProductCache(product: Product) {
+    const productId = product.id;
+    this.productCache.set(productId, product);
+    this.productsCache.forEach((productResponse) => {
+      productResponse.products = productResponse.products.map(
+        (currentProduct) =>
+          currentProduct.id === productId ? product : currentProduct
+      );
+    });
+
+    console.log('Caché actualizado');
+    
   }
 }
